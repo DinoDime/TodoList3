2df9afb1b90672fea7bb8b2882cf1881b89f9296
jess <anonymous171@posteo.de>
2021-04-15 15:05:25 -0400

    working version

--- .history/src/components/TodoList_20210415144034.js
+++ .history/src/components/TodoList_20210415144034.js
@@ -5,10 +5,10 @@ const TodoList = (todos) => {
 	return (
 		<ul>
 			{todos.map(todo => (
-				<Todo key={todo.id} todo={todo} />
+				<Todo key={todo.id} todo={todo} toggleComplete={toggleComplete}/>
 			))}
 		</ul>
 	);
 }
 
-export default TodoList
+export default TodoList;
