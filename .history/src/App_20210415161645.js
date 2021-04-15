6eb25fcc489c105ffc0eb964fbe137c4d1f49f77
jess <anonymous171@posteo.de>
2021-04-15 16:19:03 -0400

    fixed useEffect and local storage

---
+++ .history/src/App_20210415161645.js
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from "react";
+import './App.css';
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos();
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos]);
+  }
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id){
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Todo</p>
+        <TodoForm addTodo={addTodo} />
+        <TodoForm addTodo={addTodo} toggleComplete={toggleComplete}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
